67489a7dc3fe3d23ac85fc80727282b3e133584d
Loren-Wang <wangliang_workjob@163.com>
2019-12-24 22:37:54 +0800

    修改安卓样式

--- app/src/main/java/com/moments_of_life/android/activity/LoginActivity.kt
+++ app/src/main/java/com/moments_of_life/android/activity/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.moments_of_life.android.activity
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.moments_of_life.android.R
 import com.moments_of_life.android.base.BaseActivity
 import com.moments_of_life.android.utils.ToastUtils
@@ -17,6 +19,31 @@ class LoginActivity : BaseActivity() {
 
     override fun initListener() {
         super.initListener()
+        edtAccount.addTextChangedListener(object :TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+        })
+
+        btnGetVerificationCode.setOnClickListener {
+            it.isEnabled = false
+            //开始检测手机号
+            val accountInput = edtAccount.text.toString()
+            if (!accountInput.matches(phoneRegex)) {
+                it.isEnabled = true
+                ToastUtils.instance.editErrorHint(R.string.page_login_error_input_account)
+                return@setOnClickListener
+            }
+            showBaseLoading(false)
+
+        }
         btnLogin.setOnClickListener {
             it.isEnabled = false
             //开始检测手机号
